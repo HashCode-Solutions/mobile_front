19bafca10156410cb58eee8a594254404bc40710
Kavindu199729 <kavindupraneeth@kiu.ac.lk>
2023-04-14 13:11:54 +0530

    up

--- screens/ForgetPassword.js
+++ screens/ForgetPassword.js
@@ -6,139 +6,159 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
-  Platform,Image,TextInput,TouchableWithoutFeedback
+  Platform, Image, TextInput, TouchableWithoutFeedback
 } from 'react-native';
 
+function ForgetPassword({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
 
-
-
-function ForgetPassword({navigation}) {
-    const [email, setEmail] = useState('');
-    const [emailError, setEmailError] = useState('');
-
-    const handleEmailChange=(text)=>{
-        setEmail(text);
-        if (!text) {
-          setEmailError('Email is required');
-        } else {
-            setEmailError('');
-        }
+  const handleEmailChange = (text) => {
+    setEmail(text);
+    if (!text) {
+      setEmailError('Email is required');
+    } else {
+      setEmailError('');
     }
-    const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);//false means showing pasword
-    const [passwordError, setPasswordError] = useState('');
+  }
+  const [newPassword, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);//false means showing pasword
+  const [passwordError, setPasswordError] = useState('');
 
-    const passwordImage = showPassword
-    ? require('../assets/password-show.png')//if true
+  const passwordImage = showPassword
+    ? require('../assets/password-show.png')//if false
     : require('../assets/password-hide.png')//otherwise
-  
-    const toggleShowPassword = (text) => {
-        setPassword(text);
-        if(!text){
-setPasswordError('Password is required');
-        }else{
-            setPasswordError('');
-        }
-      setShowPassword(!showPassword);
-    };
-      //confirm password
 
-      const [confirmpassword, confirmsetPassword] = useState('');
-      const [confirmshowPassword, confirmsetShowPassword] = useState(false);//false means showing pasword
-      const [confirmpasswordError, confirmsetPasswordError] = useState('');
-      
+  const toggleShowPassword = (text) => {
+    setPassword(text);
+    if (!text) {
+      setPasswordError('Password is required');
+    } else {
+      setPasswordError('');
+    }
+    setShowPassword(!showPassword);
+  };
+  //confirm password
 
-      const confirmpasswordImage = confirmshowPassword
-    ? require('../assets/password-show.png')//if true
+  const [confirmpassword, confirmsetPassword] = useState('');
+  const [confirmshowPassword, confirmsetShowPassword] = useState(false);//false means showing pasword
+  const [confirmpasswordError, confirmsetPasswordError] = useState('');
+
+
+  const confirmpasswordImage = confirmshowPassword
+    ? require('../assets/password-show.png')//if false
     : require('../assets/password-hide.png')//otherwise
-      
-      
-      const toggleConfirmShowPassword = (text) => {
-        confirmsetPassword(text);
-        if(!text){
-confirmsetPasswordError('Password is required');
-        }else{
-            confirmsetPasswordError('');
-        }
-      confirmsetShowPassword(!confirmshowPassword);
 
-    };
 
-    const[checkequal,setcheckequal]=useState('');
-const checkPasswordsAreTheSame=()=>{
-if(setPassword==confirmsetPassword){
-setcheckequal('');
-}else{
-  setcheckequal('Passwords are not equal');
-}
-}
+  const toggleConfirmShowPassword = (text) => {
+    confirmsetPassword(text);
+    if (!text) {
+      confirmsetPasswordError('Password is required');
+    } else {
+      confirmsetPasswordError('');
+    }
+    confirmsetShowPassword(!confirmshowPassword);
+
+  };
+
+  const [checkequal, setcheckequal] = useState('');
+  const checkPasswordsAreTheSame = () => {
+    if (newPassword == confirmpassword) {
+      setcheckequal('');
+    } else {
+      setcheckequal('Passwords are not equal');
+    }
+  }
+
+  {/*fetch api for forget password*/}
+  const createNewPassword = async () => {
+      try {
+        {/*const response = await fetch(
+          `https://mobileback-diwisawi-production.up.railway.app/user/forget-password/`+email,{
+            method: 'PUT',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              password: newPassword,
+            }),
+          }
+        );*/}
+        console.log("clicked");
+      } catch (error) {
+        console.error(error);
+      }
+  };
+
+
   return (
     <ScrollView>
       <View style={styles.container}>
         <Text style={styles.topic}>Create a new password</Text>
 
-         <TextInput
-            style={styles.input}
-            placeholder="Email"
-            keyboardType="text"
-            placeholderTextColor="#000"
-            value={email}
-            onChangeText={handleEmailChange}
-          />
-<Text style={{ color: 'red' }}>{emailError}</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          keyboardType="text"
+          placeholderTextColor="#000"
+          value={email}
+          onChangeText={handleEmailChange}
+        />
+        <Text style={{ color: 'red' }}>{emailError}</Text>
 
-<View style={{ marginHorizontal: 25, marginBottom: 20 }}>
-<View style={{ flexDirection: 'row', alignItems: 'center', borderWidth: 1, height: 60 }}>
-      <TextInput
-        style={{ flex: 1, padding: 10 , color: '#000'}}
-        placeholder="New Password"
-        placeholderTextColor="#000"
-        keyboardType="text"
-        secureTextEntry={!showPassword}
-        value={password}
-        onChangeText={setPassword}
-      />
-      <TouchableWithoutFeedback onPress={toggleShowPassword}>
-        <View style={{ padding: 10 }}>
-        <Image  source={passwordImage} style={{ width: 40, height: 24 }} />
+        <View style={{ marginHorizontal: 25, marginBottom: 20 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', borderWidth: 1, height: 60 }}>
+            <TextInput
+              style={{ flex: 1, padding: 10, color: '#000' }}
+              placeholder="New Password"
+              placeholderTextColor="#000"
+              keyboardType="text"
+              secureTextEntry={!showPassword}
+              value={newPassword}
+              onChangeText={setPassword}
+            />
+            <TouchableWithoutFeedback onPress={toggleShowPassword}>
+              <View style={{ padding: 10 }}>
+                <Image source={passwordImage} style={{ width: 40, height: 24 }} />
+              </View>
+            </TouchableWithoutFeedback>
+          </View></View>
+        <Text style={{ color: 'red' }}>{passwordError}</Text>
+
+
+
+        {/* Confirm Password */}
+        <View style={{ marginHorizontal: 25, marginBottom: 20 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', borderWidth: 1, height: 60 }}>
+            <TextInput
+              style={{ flex: 1, padding: 10, color: '#000' }}
+              placeholder="Confirm Password"
+              placeholderTextColor="#000"
+              keyboardType="text"
+              secureTextEntry={!confirmshowPassword}
+              value={confirmpassword}
+              onChangeText={confirmsetPassword}
+            />
+            <TouchableWithoutFeedback onPress={toggleConfirmShowPassword}>
+              <View style={{ padding: 10 }}>
+                <Image source={confirmpasswordImage} style={{ width: 40, height: 24 }} />
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
         </View>
-      </TouchableWithoutFeedback>
-    </View></View>
-    <Text style={{ color: 'red' }}>{passwordError}</Text>
+        <Text style={{ color: 'red' }}>{confirmpasswordError}</Text>
+        <Text style={{ color: 'red' }}>{checkequal}</Text>
 
 
+        {/* Buttons */}
 
-   {/* Confirm Password */}
-    <View style={{ marginHorizontal: 25, marginBottom: 20 }}>
-  <View style={{ flexDirection: 'row', alignItems: 'center', borderWidth: 1, height: 60 }}>
-    <TextInput
-      style={{ flex: 1, padding: 10 , color: '#000'}}
-      placeholder="Confirm Password"
-      placeholderTextColor="#000"
-      keyboardType="text"
-      secureTextEntry={!confirmshowPassword}
-      value={confirmpassword}
-      onChangeText={confirmsetPassword}
-    />
-    <TouchableWithoutFeedback onPress={toggleConfirmShowPassword}>
-      <View style={{ padding: 10 }}>
-        <Image source={confirmpasswordImage} style={{ width: 40, height: 24 }} />
-      </View>
-    </TouchableWithoutFeedback>
-  </View>
-</View>
-<Text style={{ color: 'red' }}>{confirmpasswordError}</Text>
-<Text style={{ color: 'red' }}>{checkequal}</Text>
-
-
-    {/* Buttons */}
-
-           <TouchableOpacity style={styles.button} >
-            <Text style={{fontSize: 24, color: '#fff'}}>Reset Password</Text>
-          </TouchableOpacity>
-           <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
-            <Text style={{fontSize: 24, color: '#fff'}}>Go Back</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={createNewPassword}>
+          <Text style={{ fontSize: 24, color: '#fff' }}>Reset Password</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+          <Text style={{ fontSize: 24, color: '#fff' }}>Go Back</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -150,7 +170,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 40,
-    
+
   },
   topic: {
     margin: 24,
@@ -174,7 +194,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginHorizontal: 25,
     height: 60,
-     marginBottom: 20,
+    marginBottom: 20,
     borderRadius: 5,
   },
 });
