8a97c282ddc463e6e2e855a69e7ad83fbd5b3f49
Kavindu199729 <kavindupraneeth@kiu.ac.lk>
2023-04-18 10:16:47 +0530

    New screen

---
+++ screens/UpdateUser.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Platform, Image, TextInput, TouchableWithoutFeedback, Alert
+} from 'react-native';
